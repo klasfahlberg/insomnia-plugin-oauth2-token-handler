60fdf37b9394c94f72611b15e9aaeda112c5650c
klasfahlberg <klas.fahlberg@umu.se>
2025-03-20 10:15:51 +0100

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,51 +1,43 @@
 // For help writing plugins, visit the documentation to get started:
 //   https://support.insomnia.rest/article/26-plugins
 
-let storage = {
-    scopes: {},
+const defaultTokenSuffix = '/oauth2/v2.0/token';
 
-    getScope(environment) {
-        let scopes = this.scopes
-        let scopeKey = environment.getEnvironmentId()
-        if (scopeKey in scopes) {
-            return scopes[scopeKey]
-        }
-        else {
-            return scopes[scopeKey] = {
-                token: 'no token set'
-            }
-        }
+const storage = {
+    _tokens: {},
+    getToken(environment) {
+        return this._tokens[environment.getEnvironmentId()] || null;
+    },
+    setToken(environment, token) {
+        this._tokens[environment.getEnvironmentId()] = token;
     }
 }
 
-let tokenTag = {
+const tokenTag = {
     name: 'access_token',
     displayName: 'access_token',
-    description: 'Use the last found OAuth2 token form this environment',
+    description: 'Use the last found OAuth2 token from this environment',
     args: [],
     async run(arg, regexp, property) {
-        let scope = storage.getScope(arg.context)
-
-        return scope.token
+        console.log('[oauth2-token-provider]', 'get access_token')
+        return storage.getToken(arg.context)
     }
 }
 
-let tokenHook = function (arg) {
-    let request = arg.request
-    let requestURI = new URL(request.getUrl())
-    let environment = request.getEnvironment()
-    let endsWith = (typeof environment.oauth_token_finder_endsWith === 'undefined') ? "/oauth2/v2.0/token" : environment.oauth_token_finder_endsWith
-    if (requestURI.href.endsWith(endsWith)) {
-        let response = arg.response
-        let responseBody = response.getBody()
-        
-        let responseContent = JSON.parse(responseBody.toString('utf-8'))
-        
-        let scope = storage.getScope(environment)
+const tokenHook = async function (arg) {
+    const requestURI = new URL(arg.request.getUrl())
+    const environment = arg.request.getEnvironment()
 
-        scope.token = responseContent.access_token
+    if (requestURI.href.endsWith(environment.oauth2_token_url_suffix || defaultTokenSuffix)) {
 
-        console.log('[oauth-token-finder]', 'detected new token')
+        const data = JSON.parse((await arg.response.getBody()).toString('utf-8'))
+
+        if (typeof data.access_token !== 'undefined') {
+            storage.setToken(environment, data.access_token);
+            console.log('[oauth2-token-provider]', 'saved access_token')
+        } else {
+            console.error('[oauth2-token-provider]', `missing access_token from ${requestURI}`)
+        }
     }
 };
 
@@ -54,4 +46,4 @@ module.exports = {
     templateTags: [tokenTag],
 }
 
-console.log('[oauth-token-finder]', 'loaded')
+console.log('[oauth2-token-provider]', 'plugin loaded')
